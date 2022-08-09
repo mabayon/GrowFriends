6708311fcf16d259f2688e108f1943edf0e9fa47
WMabayon <mathieu@worklife.io>
2022-08-09 22:23:50 +0200

    Setup project

---
+++ GrowFriend/supportingFiles/CuckooFileListChecker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+whotest[0]='test' || (echo 'Failure: arrays not supported in this version of bash.' && exit 2)
+
+# Please don't forget to sort by filename
+testableFiles=(
+)
+
+for file in ${testableFiles[@]}; do
+    filePath= find $(pwd) -type f -path "*/GrowFriend/*" -name $file
+    paths="${paths} ${filePath}"
+    if [ -z "$paths" ];
+    then
+        >&2 echo "$file not found. Remove it or check the case"
+        exit 3
+    fi
+done
+echo $paths
